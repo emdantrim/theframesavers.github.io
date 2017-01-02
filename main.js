43c7412a50bc60a929cdc62f6a043cb7f7dcfc8d
Chad Waters <authorblues@gmail.com>
2017-01-02 05:58:16 -0500

    New logo, new layout, in time for AGDQ 2017

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ function makeSoundcloudEmbed(track, w)
 	embed.attr('width', w).attr('height', 166);
 	embed.attr('scrolling', 'no');
 	embed.attr('frameborder', 'no');
-	
+
 	var url = 'https%3A//api.soundcloud.com/tracks/' + track;
 	var xtra = '&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_artwork=false';
-	
+
 	// replace the player if the size is too small
 	if (w < 500)
 	{
 		xtra = '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';
-		embed.attr('height', w);	
+		embed.attr('height', w);
 	}
-	
+
 	embed.attr('src', 'https://w.soundcloud.com/player/?url=' + url + xtra);
 	return embed;
 }
@@ -23,7 +23,7 @@ function loadRecentEpisode(data)
 {
 	if (!data || !data.length) return;
 	var w = $('#main-container').innerWidth();
-	
+
 	$('#recent-ep').empty().append(makeSoundcloudEmbed(data[0].id, w));
 	$('#recent-ep-label').text(data[0].title);
 	$('#recent-ep-toggle').show();
